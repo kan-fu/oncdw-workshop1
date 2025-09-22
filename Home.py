85f3f515c0f1b6a73dda1986f09d54549ced9ae1
Kan Fu <fukan.thu@gmail.com>
2025-09-22 15:33:40 -0700

    dummy

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Hello, Streamlit! 👋')
+st.write('Welcome to my :red[first] web app!')
+
---
+++ pages/Page.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Hello, Streamlit! 👋')
+st.write('Welcome to my :red[second] web app!')
+
